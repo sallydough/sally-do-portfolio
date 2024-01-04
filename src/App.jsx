0ee393d6a583d9429b57e2a66f23016f07467b8c
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-04 05:47:22 -0700

    add components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
 import './App.css'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
+import Experience from './components/Experience/Experience'
+import Footer from './components/Footer/Footer'
+import Nav from './components/Nav/Nav'
+import Projects from './components/Projects/Projects'
 
 function App() {
 
 
   return (
     <>
-    
+    <Nav />
+    <About />
+    <Projects />
+    <Experience />
+    <Contact />
+    <Footer />
+
     <h1>Sally Do Portfolio</h1>
     </>
   )
