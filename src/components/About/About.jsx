b3a555f9a8d521cc1748c28c88546481a66e5c03
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-05 11:12:14 -0700

    add about components

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,10 +1,33 @@
-import React from 'react'
-import './about.css'
+import React from "react";
+import "./about.css";
 
 const About = () => {
   return (
-    <div>About</div>
-  )
-}
+    <>
+      <section className="about-container">
+        <div className="bio-content">
+          <div className="about-title">
+            <h1>full stack web developer & ui/ux designer</h1>
+          </div>
+          <div className="about-paragraph">
+            <p>
+              HI MY NAME IS IS SALLY. IT'S MUCH, MUCH BETTER TO WIND UP WITH A
+              LOT OF CRAP HAVING TRIED THAN TO OVERTHINK IN THE BEGINNING AND
+              NOT DO IT.
+            </p>
+          </div>
 
-export default About
+          <div className="arrow-connect-button">
+            <div className="about-arrow">↓</div>
+            <div className="connect-button">
+              <h1>let's work together ➛</h1>
+            </div>
+          </div>
+          <div className="about-picture">Picture</div>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default About;
