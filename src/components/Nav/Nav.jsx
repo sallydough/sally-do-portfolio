806ae0d883422710ed18fe78e83124d5b71c081b
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-05 11:01:44 -0700

    created nav bar components

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -2,7 +2,27 @@ import './nav.css'
 
 const Nav = () => {
   return (
-    <div>Nav</div>
+    <>
+    <section id="nav-container">
+      <div className="about-projects">
+        <h2>ABOUT</h2>
+        <h1 className="dash-nav">—</h1>
+        <h2>PROJECTS</h2>
+      </div>
+      <div className="name">
+        <h2>Sally Do</h2>
+      </div>
+      <div className="experience-contact">
+        <h2>EXPERIENCE</h2>
+        <h1 className="dash-nav">—</h1>
+        <h2>CONTACT</h2>
+      </div>
+    </section>
+    
+    
+    
+    </>
+    
   )
 }
 
