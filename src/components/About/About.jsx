b098e2783c3c6912847edde750d2981390bf5286
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-09 06:02:44 -0700

    changed font

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -7,7 +7,7 @@ const About = () => {
         <div className="bio-content">
           <div className="about-title">
             <h1>full stack web developer &</h1>
-            <h1>ui/ux designer</h1>
+            <h1 className="ui-ux">ui/ux designer</h1>
           </div>
           <div className="about-paragraph">
             <p>
