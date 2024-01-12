cda29534b39e719f381b94309019c5f272fe9661
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-11 23:36:25 -0700

    got about page identical to figma

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -6,14 +6,13 @@ const About = () => {
       <section id="about-container">
         <div className="bio-content">
           <div className="about-title">
-            <h1>full stack web developer &</h1>
+            <h1 className="dev">full stack web developer &</h1>
             <h1 className="ui-ux">ui/ux designer</h1>
           </div>
           <div className="about-paragraph">
             <p>
               HI MY NAME IS IS SALLY. IT'S MUCH, MUCH BETTER TO WIND UP WITH A
               LOT OF CRAP HAVING TRIED THAN TO OVERTHINK IN THE BEGINNING AND
-              NOT DO IT.  LOT OF CRAP HAVING TRIED THAN TO OVERTHINK IN THE BEGINNING AND
               NOT DO IT.
             </p>
           </div>
