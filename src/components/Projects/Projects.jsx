8e32a6fa315db471fc7791ff123cb5c5b67d60a7
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-16 11:38:48 -0700

    create github and demo button

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -10,55 +10,55 @@ const Projects = () => {
       <article className="project-card card1">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
       <article className="project-card card2">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
       <article className="project-card card3">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
       <article className="project-card card4">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
       <article className="project-card card5">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
       <article className="project-card card6">
         <figure className="project-pic">Picture</figure>
         <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
-        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <p className='description'>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
-          <button>GITHUB</button>
-          <button>LIVE DEMO</button>
+          <button className='button-github'>GITHUB</button>
+          <button className='button-demo'>LIVE DEMO</button>
         </div>
       </article>
     </main>
