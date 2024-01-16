d63c982eceb8428b8422e49e6d1ba36b2b6f654f
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-16 11:10:13 -0700

    changed profile pic position

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -9,7 +9,7 @@ const Projects = () => {
     <main className="project-card-container">
       <article className="project-card card1">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
@@ -18,7 +18,7 @@ const Projects = () => {
       </article>
       <article className="project-card card2">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
@@ -27,7 +27,7 @@ const Projects = () => {
       </article>
       <article className="project-card card3">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
@@ -36,7 +36,7 @@ const Projects = () => {
       </article>
       <article className="project-card card4">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
@@ -45,7 +45,7 @@ const Projects = () => {
       </article>
       <article className="project-card card5">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
@@ -54,7 +54,7 @@ const Projects = () => {
       </article>
       <article className="project-card card6">
         <figure className="project-pic">Picture</figure>
-        <h1>MENSTRUAL TRACKING APP</h1>
+        <h1 className='project-name'>MENSTRUAL TRACKING APP</h1>
         <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
         <div className="github-demo">
           <button>GITHUB</button>
