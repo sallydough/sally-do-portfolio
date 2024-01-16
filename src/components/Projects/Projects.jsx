006e303cfae5a0f52f863e325d4d27e14e2f65c7
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-01-16 10:14:08 -0700

    created project cards

--- src/components/Projects/Projects.jsx
+++ src/components/Projects/Projects.jsx
@@ -4,9 +4,36 @@ const Projects = () => {
   return (
    <section id="projects">
     <div className="project-title">
-     <h1>WORK & PROJECTS</h1> 
+     <h1>WORK & PROJECTS*</h1> 
     </div>
     <main className="project-card-container">
+      <article className="project-card">
+        <figure className="project-pic">Picture</figure>
+        <h1>MENSTRUAL TRACKING APP</h1>
+        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <div className="github-demo">
+          <button>GITHUB</button>
+          <button>LIVE DEMO</button>
+        </div>
+      </article>
+      <article className="project-card">
+        <figure className="project-pic">Picture</figure>
+        <h1>MENSTRUAL TRACKING APP</h1>
+        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <div className="github-demo">
+          <button>GITHUB</button>
+          <button>LIVE DEMO</button>
+        </div>
+      </article>
+      <article className="project-card">
+        <figure className="project-pic">Picture</figure>
+        <h1>MENSTRUAL TRACKING APP</h1>
+        <p>This app tracks your menstrual cycle to provide optimization in your overall health, activity, fasting.</p>
+        <div className="github-demo">
+          <button>GITHUB</button>
+          <button>LIVE DEMO</button>
+        </div>
+      </article>
       <article className="project-card">
         <figure className="project-pic">Picture</figure>
         <h1>MENSTRUAL TRACKING APP</h1>
