91731106972d54e4839d93312c9abb72519c4766
Sally Do <56125850+sallydough@users.noreply.github.com>
2024-02-09 15:38:38 -0700

    fixed connect

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -9,7 +9,7 @@ const Contact = () => {
   return (
     <section id="contact">
       <div className="contact-form">
-        <h1 className="contact-title">let's connet</h1>
+        <h1 className="contact-title">let's connect</h1>
         {/* <div className="form-submit">
           <form action="">
             <input type="text" placeholder="Name" name="name" id="name" />
